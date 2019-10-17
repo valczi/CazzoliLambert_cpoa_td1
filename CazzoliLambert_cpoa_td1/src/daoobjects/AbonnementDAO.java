f12c201371d1bbc361bc213f28bfec8ebc712bbf
Valentin <Valentin@orange.fr>
2019-10-17 21:59:22 +0200

    Modif git

--- CazzoliLambert_cpoa_td1/src/daoobjects/AbonnementDAO.java
+++
@@ -1,8 +0,0 @@
-package daoobjects;
-
-import daofactory.*;
-import metiers.*;
-
-public interface AbonnementDAO extends DAO<AbonnementM> {
-	public abstract AbonnementM getById(int id,int id2);
-}
--- CazzoliLambert_cpoa_td1/src/daoobjects/RevueDAO.java
+++
@@ -1,9 +0,0 @@
-package daoobjects;
-
-import daofactory.*;
-import metiers.*;
-
-public interface RevueDAO extends DAO<RevueM> {
-	public abstract RevueM getById(int id);
-}
-
